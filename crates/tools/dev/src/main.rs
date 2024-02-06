6e2fb4ad57b0e79ac038d9b7b5ad561c22fd7c82
oneofthezombies <hunhoekim@gmail.com>
2024-02-06 23:44:39 +0900

    fix: clippy

--- crates/tools/dev/src/main.rs
+++ crates/tools/dev/src/main.rs
@@ -72,43 +72,37 @@ fn build(target: &str) {
     );
 
     if env::var("GITHUB_ACTIONS").is_ok() {
-        match env::var("GITHUB_OUTPUT") {
-            Ok(output) => {
-                let windows_path = Path::new("target")
-                    .join(target)
-                    .join("release")
-                    .join("kill_tree_cli.exe");
-                let file_path = if windows_path.exists() {
-                    windows_path
-                } else {
-                    Path::new("target")
-                        .join(target)
-                        .join("release")
-                        .join("kill_tree_cli")
-                };
-                let zip_path = format!("{}.zip", target);
-                let zip_file = std::fs::File::create(zip_path.clone()).unwrap();
-                let mut zip = zip::ZipWriter::new(zip_file);
-                let options = zip::write::FileOptions::default()
-                    .compression_method(zip::CompressionMethod::Stored)
-                    .unix_permissions(0o755);
-                let file_name = file_path.file_name().unwrap().to_str().unwrap();
-                zip.start_file(file_name, options).unwrap();
-                let mut file = std::fs::File::open(file_path).unwrap();
-                std::io::copy(&mut file, &mut zip).unwrap();
-                zip.finish().unwrap();
+        let output = env::var("GITHUB_OUTPUT").expect("No GITHUB_OUTPUT");
+        let windows_path = Path::new("target")
+            .join(target)
+            .join("release")
+            .join("kill_tree_cli.exe");
+        let file_path = if windows_path.exists() {
+            windows_path
+        } else {
+            Path::new("target")
+                .join(target)
+                .join("release")
+                .join("kill_tree_cli")
+        };
+        let zip_path = format!("{target}.zip");
+        let zip_file = std::fs::File::create(zip_path.clone()).unwrap();
+        let mut zip = zip::ZipWriter::new(zip_file);
+        let options = zip::write::FileOptions::default()
+            .compression_method(zip::CompressionMethod::Stored)
+            .unix_permissions(0o755);
+        let file_name = file_path.file_name().unwrap().to_str().unwrap();
+        zip.start_file(file_name, options).unwrap();
+        let mut file = std::fs::File::open(file_path).unwrap();
+        std::io::copy(&mut file, &mut zip).unwrap();
+        zip.finish().unwrap();
 
-                let mut output_path = std::fs::OpenOptions::new()
-                    .write(true)
-                    .append(true)
-                    .open(output)
-                    .unwrap();
-                writeln!(output_path, "ARTIFACT_PATH={}", zip_path).unwrap();
-            }
-            Err(_) => {
-                panic!("No GITHUB_OUTPUT");
-            }
-        }
+        let mut output_path = std::fs::OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(output)
+            .unwrap();
+        writeln!(output_path, "ARTIFACT_PATH={zip_path}").unwrap();
     }
 }
 
