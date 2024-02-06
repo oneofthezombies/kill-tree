6daf488d0058a142dbc362817218f945556c29c8
oneofthezombies <hunhoekim@gmail.com>
2024-02-06 19:55:29 +0900

    chore: update pre-push

--- crates/tools/dev/src/main.rs
+++ crates/tools/dev/src/main.rs
@@ -13,130 +13,108 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    Check,
+    Clippy,
+    Fmt,
     Build { platform: String },
-    Test { platform: String },
+    Test { platform: Option<String> },
+}
+
+fn run(program: &str, args: &[&str]) {
+    let mut command = Command::new(program);
+    command
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .args(args);
+    println!("Run {:?} {:?}", program, args);
+    command
+        .status()
+        .expect(format!("Failed to run {:?} {:?}", program, args).as_str());
+}
+
+fn check() {
+    run("cargo", &["check", "--workspace"]);
+}
+
+fn clippy() {
+    run("cargo", &["clippy", "--", "-D", "clippy::pedantic"]);
+}
+
+fn fmt() {
+    run("cargo", &["fmt", "--", "--check"]);
 }
 
 fn build(platform: &str) {
     env::set_var("RUSTFLAGS", "-C target-feature=+crt-static");
     if platform == "windows" {
-        Command::new("rustup")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["target", "add", "x86_64-pc-windows-msvc"])
-            .status()
-            .expect("Failed to add x86_64-pc-windows-msvc target");
-
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args([
+        run("rustup", &["target", "add", "x86_64-pc-windows-msvc"]);
+        run(
+            "cargo",
+            &[
                 "build",
                 "-p",
                 "kill_tree_cli",
                 "-r",
                 "--target",
                 "x86_64-pc-windows-msvc",
-            ])
-            .status()
-            .expect("Failed to build kill_tree_cli");
+            ],
+        );
     } else if platform == "linux" {
-        Command::new("rustup")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["target", "add", "x86_64-unknown-linux-musl"])
-            .status()
-            .expect("Failed to add x86_64-unknown-linux-musl target");
-
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args([
+        run("rustup", &["target", "add", "x86_64-unknown-linux-musl"]);
+        run(
+            "cargo",
+            &[
                 "build",
                 "-p",
                 "kill_tree_cli",
                 "-r",
                 "--target",
                 "x86_64-unknown-linux-musl",
-            ])
-            .status()
-            .expect("Failed to build kill_tree_cli");
+            ],
+        );
     } else if platform == "macos" {
-        Command::new("rustup")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["target", "add", "aarch64-apple-darwin"])
-            .status()
-            .expect("Failed to add aarch64-apple-darwin target");
-
-        Command::new("rustup")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["target", "add", "x86_64-apple-darwin"])
-            .status()
-            .expect("Failed to add x86_64-apple-darwin target");
-
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args([
+        run("rustup", &["target", "add", "aarch64-apple-darwin"]);
+        run("rustup", &["target", "add", "x86_64-apple-darwin"]);
+        run(
+            "cargo",
+            &[
                 "build",
                 "-p",
                 "kill_tree_cli",
                 "-r",
                 "--target",
                 "aarch64-apple-darwin",
-            ])
-            .status()
-            .expect("Failed to build kill_tree_cli");
-
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args([
+            ],
+        );
+        run(
+            "cargo",
+            &[
                 "build",
                 "-p",
                 "kill_tree_cli",
                 "-r",
                 "--target",
                 "x86_64-apple-darwin",
-            ])
-            .status()
-            .expect("Failed to build kill_tree_cli");
+            ],
+        );
     } else {
         panic!("Unsupported platform: {platform}");
     }
 }
 
-fn test(platform: &str) {
-    if platform == "windows" {
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["test", "--target", "x86_64-pc-windows-msvc"])
-            .status()
-            .expect("Failed to test kill_tree_cli");
-    } else if platform == "linux" {
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["test", "--target", "x86_64-unknown-linux-musl"])
-            .status()
-            .expect("Failed to test kill_tree_cli");
-    } else if platform == "macos" {
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["test", "--target", "aarch64-apple-darwin"])
-            .status()
-            .expect("Failed to test kill_tree_cli");
+fn test(platform: Option<String>) {
+    let Some(platform) = platform else {
+        run("cargo", &["test", "--workspace"]);
+        return;
+    };
 
-        Command::new("cargo")
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .args(["test", "--target", "x86_64-apple-darwin"])
-            .status()
-            .expect("Failed to test kill_tree_cli");
+    if platform == "windows" {
+        run("cargo", &["test", "--target", "x86_64-pc-windows-msvc"]);
+    } else if platform == "linux" {
+        run("cargo", &["test", "--target", "x86_64-unknown-linux-musl"]);
+    } else if platform == "macos" {
+        run("cargo", &["test", "--target", "aarch64-apple-darwin"]);
+        run("cargo", &["test", "--target", "x86_64-apple-darwin"]);
     } else {
         panic!("Unsupported platform: {platform}");
     }
@@ -145,12 +123,11 @@ fn test(platform: &str) {
 fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Some(Commands::Build { platform }) => {
-            build(&platform);
-        }
-        Some(Commands::Test { platform }) => {
-            test(&platform);
-        }
+        Some(Commands::Check) => check(),
+        Some(Commands::Clippy) => clippy(),
+        Some(Commands::Fmt) => fmt(),
+        Some(Commands::Build { platform }) => build(&platform),
+        Some(Commands::Test { platform }) => test(platform),
         None => {
             panic!("No command");
         }
